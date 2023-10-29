1e1a885842a0f8016b7e49303fc6a69f577a84a5
Tarsis <br.tmvdl@gmail.com>
2023-10-29 11:54:34 -0300

    Adding script files

---
+++ src/markets.candles.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/sh
+
+## inputs
+
+# . datetime.sh
+
+market_symbol="${1}"
+
+interval="${2}"
+
+. datetime.sh "${3}"
+start_time="${iso8601}"
+
+. datetime.sh "0sec"
+end_time="${iso8601}"
+
+path="markets/${market_symbol}/candles"
+
+## runners
+
+resp=$( bash get.sh "${path}" "interval=${interval}&start_time=${start_time}&end_time=${end_time}" )
+
+## outputs
+
+bash create.sh "${path}" "${datetime}" "resp" "${resp}"
