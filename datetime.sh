aa879ad141bdc72b6e4f3cd0a0d7b87add27ff5c
Tarsis <br.tmvdl@gmail.com>
2023-10-29 11:54:30 -0300

    Updating CORE script files

--- datetime.sh
+++ datetime.sh
@@ -2,22 +2,26 @@
 
 # yyyy-MM-dd HH:mm:ss
 
-year=$( date +%Y )
+sec="${1:-"0sec"}"
 
-month=$( date +%m )
+year=$( date +%Y --date="-${sec}" )
 
-day=$( date +%d )
+month=$( date +%m --date="-${sec}" )
+
+day=$( date +%d --date="-${sec}" )
 
 date="${year}-${month}-${day}"
 
-hour=$( date +%H )
+hour=$( date +%H --date="-${sec}" )
 
-minute=$( date +%M )
+minute=$( date +%M --date="-${sec}" )
 
-second=$( date +%S )
+second=$( date +%S --date="-${sec}" )
 
 time="${hour}:${minute}:${second}"
 
-timestamp="${date} ${time}"
+timestamp="${date}T${time}"
+
+iso8601="${date}T${hour}:${minute}"
 
 datetime="${year}${month}${day}${hour}${minute}${second}"
